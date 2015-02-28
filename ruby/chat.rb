a90186db71a5431e4a8e1ee3cbe67d9269547eff
root <root@debian>
2015-02-28 18:13:15 +0100

    modif deploy heroku

--- ruby/chat.rb
+++ ruby/chat.rb
@@ -3,8 +3,11 @@ require 'mongo'
 include Mongo
 
 get '/GetMessages' do
-  	"Hello World!"
+  	@title = 'Get All Messages'
 	mongo_client = MongoClient.new("ds039341.mongolab.com", 39341)
 	db = mongo_client.db("chat_ruby_db")
+	auth = db.authenticate("user", "password")
 	coll = db.collection("messages")
+ 	@messages = coll.find
+	erb :GetMessagesView
 end
