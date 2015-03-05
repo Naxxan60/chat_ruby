2db3a409b031f4984de5f18587526424fc5988e4
AbdelTRAORE <abdelismael@gmail.com>
2015-03-05 15:49:26 +0100

    Ajout utilisateur

--- chat.rb
+++ chat.rb
@@ -9,9 +9,10 @@ end
 
 get '/GetMessages' do
   	@title = 'Get All Messages'
-	mongo_client = MongoClient.new("ds039341.mongolab.com", 39341)
+	mongo_client = MongoClient.new("localhost", 27017) 
+	#mongo_client = MongoClient.new("ds039341.mongolab.com", 39341)
 	db = mongo_client.db("chat_ruby_db")
-	auth = db.authenticate("user", "password")
+	#auth = db.authenticate("user", "password")
 	coll = db.collection("messages")
  	@messages = coll.find
 	erb :GetMessagesView
@@ -22,9 +23,10 @@ get '/SendMessage'do
 end 
 
 post '/SendMessage'do
-	mongo_client = MongoClient.new("ds039341.mongolab.com", 39341)
+	mongo_client = MongoClient.new("localhost", 27017) 
+	#mongo_client = MongoClient.new("ds039341.mongolab.com", 39341)
 	db = mongo_client.db("chat_ruby_db")
-	auth = db.authenticate("user", "password")
+	#auth = db.authenticate("user", "password")
 	coll = db.collection("messages")
 
 	autor = params[:autor]
@@ -33,4 +35,23 @@ post '/SendMessage'do
 	doc ={"autor" => autor, "message" => message}
 	coll.insert(doc)
 	erb :MessageSentView
+end 
+
+get '/SignUp'do
+	erb :SignUpView
+end 	
+
+post '/SignUp'do
+	mongo_client = MongoClient.new("localhost", 27017) 
+	#mongo_client = MongoClient.new("ds039341.mongolab.com", 39341)
+	db = mongo_client.db("chat_ruby_db")
+	#auth = db.authenticate("user", "password")
+	coll = db.collection("users")
+
+	login = params[:login]
+	password = params[:password]
+
+	doc1 ={"login" => login, "password" => password}
+	coll.insert(doc1)
+	erb :SignUpSentView
 end 
